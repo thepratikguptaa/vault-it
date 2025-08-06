76c7cc9b73d24a032ae95ee80286c3af9e003307
Pratik Gupta <pratikguptamain@gmail.com>
2025-08-06 16:59:22 +0530

    installed lucide react

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ const {heroui} = require("@heroui/theme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/@heroui/theme/dist/components/(card|ripple).js",
-  ],
+    "./node_modules/@heroui/theme/dist/components/(button|card|divider|input|ripple|spinner|form).js"
+],
   theme: {
     extend: {},
   },
