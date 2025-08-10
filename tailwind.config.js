da59d48c4634f84bf3dd936e3185078340070666
Pratik Gupta <pratikguptamain@gmail.com>
2025-08-10 11:35:36 +0530

    installed heroui components

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const {heroui} = require("@heroui/theme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/@heroui/theme/dist/components/(button|card|divider|input|ripple|spinner|form).js"
+    "./node_modules/@heroui/theme/dist/components/(avatar|button|card|divider|dropdown|input|navbar|ripple|spinner|menu|popover|form).js"
 ],
   theme: {
     extend: {},
